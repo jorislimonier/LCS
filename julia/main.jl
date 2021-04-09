5ce8561b9e68e8deb5e92f96df9c5711c50f98d5
jorislimonier <joris.limonier@hotmail.fr>
2021-04-09 19:26:39 +0200

    now do moving averages

--- julia/main.jl
+++ julia/main.jl
@@ -7,3 +7,14 @@ include("Gadfly.jl")
 ## plot all given averages
 res = Sequences.lcs_average_lengths_comparison(100:100:500, 100)
 PlotlyJS.average_lcs_length(res)
+
+# continue with moving averages
+sorted_keys = sort([collect(res)[i][1] for i in 1:length(res)])
+moving_averages = Dict()
+for key in sorted_keys
+    all_lengths = res[key]
+    for i in 1:length(all_lengths)
+        println(len)
+    end
+end
+
