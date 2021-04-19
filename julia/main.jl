f623b9a9ded914c5d4a7d753c9c64bd5828ddd41
jorislimonier <joris.limonier@hotmail.fr>
2021-04-19 12:29:47 +0200

    files cleaning

--- julia/main.jl
+++ julia/main.jl
@@ -1,35 +1,13 @@
 include("Sequences.jl")
 include("PlotlyVisualization.jl")
-include("GadflyPlots.jl")
 
-seq_length = 500
-runs = 100
-nb_chars = 3
-results = Sequences.multiple_lcs_lengths(seq_length, runs, nb_chars)
+seq_length = 500 ; seq_lengths = 0:500:2000 ; runs = 2 ; nb_chars = 2
+mult_lcs_lengths = Sequences.multiple_lcs_lengths(seq_length, runs, nb_chars)
+lcs_av = Sequences.compare_lcs_averages(seq_lengths, runs, nb_chars)
 
-# plot moving averages
-moving_averages = Sequences.moving_averages(results)
-PlotlyVisualization.average_lcs_length(moving_averages, seq_length)
+# # plot moving averages
+# moving_averages = Sequences.moving_averages(mult_lcs_lengths)
+# PlotlyVisualization.average_lcs_length(moving_averages, seq_length)
 
 # compare lcs averages
-plot_averages = PlotlyVisualization.plot_average_lengths(0:100:1000, 2, nb_chars)
-
-
-using DataFrames, PlotlyJS, Statistics
-
-lcs_averages = Sequences.compare_lcs_averages(000:1000:5000, 3, nb_chars)
-sorted_seq_lengths = sort(collect(keys(lcs_averages)))
-sorted_lcs_averages = [lcs_averages[k] for k in sorted_seq_lengths]
-
-df = DataFrame(seq_lengths=sorted_seq_lengths, lcs_averages=sorted_lcs_averages)
-
-df[:,:ratio] = df[:, 2] ./ df[:, 1]
-df[1, :ratio] = 0.
-df
-slope = mean(df[2:end, :ratio])
-plot = Plot()
-addtraces!(plot, scatter(x=df[:,"seq_lengths"], y=df[:,"lcs_averages"]))
-x = 0:5000
-addtraces!(plot, scatter(x=x, y=slope .* x))
-
-plot
+plot_averages = PlotlyVisualization.plot_average_lengths(lcs_av)
